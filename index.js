d560b14ead24209e995ac490e20a076e2a8c587e
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-12-09 22:26:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'a poor unfortunate soul';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another poor unfortunate soul';
+}
